d7cc5e0a8515ccc5a4633186015394b4546a746a
Sarah Guthrie <sguthrie@mgh.harvard.edu>
2023-04-14 15:30:20 -0400

    Fix import error

--- run_search_on_roi_gamma_model.py
+++ run_search_on_roi_gamma_model.py
@@ -24,8 +24,11 @@ from feeg_fmri_sync import SEARCH_TYPES
 from feeg_fmri_sync.constants import PLOT_ALPHA, PLOT_DELTA, PLOT_TAU
 from feeg_fmri_sync.io import load_roi_from_mat
 from feeg_fmri_sync.models import EEGData, fMRIData
-from feeg_fmri_sync.plotting import plot_all_search_results_2d_on_diff_colormaps, \
-    plot_eeg_hdr_across_delta_tau_alpha_range, save_eeg_hdr_across_delta_tau_alpha_range, save_plot
+from feeg_fmri_sync.plotting import (
+    plot_all_search_results_2d_on_diff_colormaps,
+    plot_eeg_hdr_across_delta_tau_alpha_range,
+    save_plot
+)
 from feeg_fmri_sync.search import search_voxels
 
 
