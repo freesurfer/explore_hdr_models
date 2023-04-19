8ab7f1ba434979c2e061961a44ae36bae7d20458
Sarah Guthrie <sguthrie@mgh.harvard.edu>
2023-04-19 11:55:54 -0400

    Fix syntax errors and remove commented out print statements

--- feeg_fmri_sync/models.py
+++ feeg_fmri_sync/models.py
@@ -18,11 +18,6 @@ from feeg_fmri_sync.utils import (
 )
 
 
-class GridSearch:
-    def __init__(self, param_names):
-        self.params = param_names
-
-
 class HemodynamicModel:    
     def __init__(self, eeg: EEGData, fmri: fMRIData, name: str, n_tr_skip_beg: int = 1,
                  hemodynamic_response_window: float = 30, display_plot: bool = True,
@@ -30,7 +25,6 @@ class HemodynamicModel:
         # Data
         self.eeg: EEGData = eeg
         self.fmri: fMRIData = fMRIData(data=fmri.data.squeeze(), TR=fmri.TR)
-        #print(f'Num eeg nans: {np.count_nonzero(np.isnan(self.eeg.data))}')
 
         # Ratio of eeg freq to fMRI frequency
         self.r_fmri: float = get_ratio_eeg_freq_to_fmri_freq(self.eeg.sample_frequency, self.fmri.TR)
@@ -83,8 +77,8 @@ class HemodynamicModel:
         time_steps_for_eeg = np.arange(len(self.eeg.data)) / self.eeg.sample_frequency
         time_steps_for_fmri = time_steps_for_eeg[::self.r_fmri]
         if residual is not None:
-            X_nan = np.isnan(est_fmri)
-            plt.plot(time_steps_for_fmri[~X_nan], residual, '.', label='Residual')
+            x_nan = np.isnan(est_fmri)
+            plt.plot(time_steps_for_fmri[~x_nan], residual, '.', label='Residual')
             plt.xlabel(f'Residual Variance is {residual_variance:.6f}')
         plt.plot(time_steps_for_fmri, actual_fmri, label='Actual fMRI')
         plt.plot(time_steps_for_fmri, est_fmri, label='Estimated fMRI')
@@ -92,7 +86,8 @@ class HemodynamicModel:
         if not actual_fmri_name:
             title = f'Estimated fMRI HDR from EEG spikes compared with actual fMRI using model={self.name}'
         else:
-            title = f'Estimated fMRI HDR from EEG spikes compared with actual fMRI ({actual_fmri_name}) using model={self.name}'
+            title = f'Estimated fMRI HDR from EEG spikes compared with actual fMRI ({actual_fmri_name}) ' \
+                    f'using model={self.name}'
         plt.title(title)
         plt.legend()
         if display:
@@ -113,7 +108,8 @@ class HemodynamicModel:
         """
         return get_est_hemodynamic_response(self.get_time_steps(), delta, tau, alpha)
 
-    def get_est_fmri_hemodynamic_response(self, est_hemodynamic_response: npt.NDArray) -> Tuple[npt.NDArray, npt.NDArray]:
+    def get_est_fmri_hemodynamic_response(self,
+                                          est_hemodynamic_response: npt.NDArray) -> Tuple[npt.NDArray, npt.NDArray]:
         hemodynamic_response_to_eeg = get_hdr_for_eeg(self.eeg.data, est_hemodynamic_response)
         est_fmri = downsample_hdr_for_eeg(self.r_fmri, hemodynamic_response_to_eeg)
         return hemodynamic_response_to_eeg, est_fmri
@@ -127,11 +123,13 @@ class HemodynamicModel:
             self.transform_actual_fmri = lambda x: x[self.n_tr_skip_beg:]
             if est_fmri.size > self.fmri.data.size - self.n_tr_skip_beg:
                 if self.plot:
-                    print(f'Estimated fMRI is larger than actual fMRI (-# skipped TRs at beginning of EEG): {est_fmri.size} : {self.fmri.data.size} - {self.n_tr_skip_beg}')
+                    print(f'Estimated fMRI is larger than actual fMRI (-# skipped TRs at beginning of EEG): '
+                          f'{est_fmri.size} : {self.fmri.data.size} - {self.n_tr_skip_beg}')
                 self.transform_est_fmri = lambda x: x[:self.fmri.data.size]
             if est_fmri.size < self.fmri.data.size - self.n_tr_skip_beg:
                 if self.plot:
-                    print(f'Estimated fMRI is smaller than actual fMRI (-# skipped TRs at beginning of EEG): {est_fmri.size} : {self.fmri.data.size} - {self.n_tr_skip_beg}')
+                    print(f'Estimated fMRI is smaller than actual fMRI (-# skipped TRs at beginning of EEG): '
+                          f'{est_fmri.size} : {self.fmri.data.size} - {self.n_tr_skip_beg}')
                 self.transform_actual_fmri = lambda x: x[self.n_tr_skip_beg:est_fmri.size + self.n_tr_skip_beg]
         return self.transform_est_fmri, self.transform_actual_fmri
 
@@ -147,25 +145,21 @@ class HemodynamicModel:
         """
         if self.plot:
             print(f'Is est_fmri close to actual_fmri? {np.isclose(est_fmri, actual_fmri)}')
-        X_nan = np.isnan(est_fmri)
-        X_drop_nans = np.extract(~X_nan, est_fmri)
-        y_drop_nans_t = np.extract(~X_nan, actual_fmri)
+        x_nan = np.isnan(est_fmri)
+        x_drop_nans = np.extract(~x_nan, est_fmri)
+        y_drop_nans_t = np.extract(~x_nan, actual_fmri)
         # ones not necessary here
-        X_t = np.array([X_drop_nans, np.ones(X_drop_nans.size)])
-        beta = np.matmul(np.matmul(np.linalg.inv(np.matmul(X_t, X_t.T)), X_t), y_drop_nans_t.T)
-        y_hat = np.matmul(X_t.T, beta)
+        x_t = np.array([x_drop_nans, np.ones(x_drop_nans.size)])
+        beta = np.matmul(np.matmul(np.linalg.inv(np.matmul(x_t, x_t.T)), x_t), y_drop_nans_t.T)
+        y_hat = np.matmul(x_t.T, beta)
         residual = np.subtract(y_drop_nans_t, y_hat)
-        degrees_of_freedom = X_t.shape[1] - X_t.shape[0]
+        degrees_of_freedom = x_t.shape[1] - x_t.shape[0]
         residual_variance = np.sum(residual**2) / degrees_of_freedom
         return beta, residual, residual_variance, degrees_of_freedom
 
     def score_from_hemodynamic_response(self, est_hemodynamic_response):
         hemodynamic_response_to_eeg, est_fmri = self.get_est_fmri_hemodynamic_response(est_hemodynamic_response)
         if self.plot:
-            #print(f'Num nans in hemodynamic response to eeg: {np.count_nonzero(np.isnan(hemodynamic_response_to_eeg))}')
-            #print(f'length of eeg: {self.eeg.data.size}')
-            #print(f'r_fmri {self.r_fmri}, length of hemodynamic_response: {hemodynamic_response_to_eeg.size}, '
-            #      f'hemodynamic_response/r_fmri: {hemodynamic_response_to_eeg.size / self.r_fmri} length of fmri data: {self.fmri.data.size}')
             self.plot_hdr_for_eeg(hemodynamic_response_to_eeg, est_fmri)
         if self.save_plot_dir:
             self.plot_hdr_for_eeg(hemodynamic_response_to_eeg, est_fmri, display=False)
